4e938d680b033fb714c114ac98f19b0acb8ae22a
Patric Poba <poba.dev@outlook.com>
2019-05-05 18:23:28 +0000

    Apply fixes from StyleCI

--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -1,34 +1,34 @@
 <?php
 
-use Mpociot\ChuckNorrisJokes\JokeFactory;
 use PHPUnit\Framework\TestCase;
- 
+use Mpociot\ChuckNorrisJokes\JokeFactory;
+
 class JokeFactoryTest extends TestCase
 {
-	/** @test */
-	function it_returns_a_random_joke()
-	{
-		$jokes = new JokeFactory(['This is a joke']);
+    /** @test */
+    public function it_returns_a_random_joke()
+    {
+        $jokes = new JokeFactory(['This is a joke']);
 
-		$joke = $jokes->getRandomJoke();
+        $joke = $jokes->getRandomJoke();
 
-		$this->assertSame('This is a joke', $joke);
-	}
+        $this->assertSame('This is a joke', $joke);
+    }
 
-	/** @test */
-	function it_returns_a_predefined_joke()
-	{
-		$chuchJokes = [
-			'The First rule of Chuck Norris is: you do not talk about Chuck Norris.', 
-			'Chuck Norris does not wear a condom. Because there is no such thing as protection from Chuck Norris.',	 
-			'Chuck Norris\' tears cure cancer. Too bad he has never cried.',	 
-			'Chuck Norris counted to infinity... Twice.'
-		];
+    /** @test */
+    public function it_returns_a_predefined_joke()
+    {
+        $chuchJokes = [
+            'The First rule of Chuck Norris is: you do not talk about Chuck Norris.',
+            'Chuck Norris does not wear a condom. Because there is no such thing as protection from Chuck Norris.',
+            'Chuck Norris\' tears cure cancer. Too bad he has never cried.',
+            'Chuck Norris counted to infinity... Twice.',
+        ];
 
-		$jokes = new JokeFactory($chuchJokes);
+        $jokes = new JokeFactory($chuchJokes);
 
-		$joke = $jokes->getRandomJoke();
+        $joke = $jokes->getRandomJoke();
 
-		$this->assertContains($joke, $chuchJokes);
-	}
+        $this->assertContains($joke, $chuchJokes);
+    }
 }
