543b075773ef5140493e26cfe8e925726e67d3fc
PatricPoba <poba.dev@outlook.com>
2019-05-11 20:55:23 +0000

    Add laravel Command to generate jokes

--- src/ChuckNorrisJokesServiceProvider.php
+++ src/ChuckNorrisJokesServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Mpociot\ChuckNorrisJokes;
 
 use Illuminate\Support\ServiceProvider;
+use Mpociot\ChuckNorrisJokes\Console\ChuckNorrisJoke;
 use Mpociot\ChuckNorrisJokes\JokeFactory;
 
  
@@ -11,7 +12,11 @@ class ChuckNorrisJokesServiceProvider extends ServiceProvider
 	
 	public function boot()
 	{
-		# code...
+		if ($this->app->runningInConsole()) {
+			$this->commands([
+					ChuckNorrisJoke::class
+			]);
+		}
 	}
 
 	public function register()
